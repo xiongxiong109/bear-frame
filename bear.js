93c92e078957a357de4804cfbf0e104908963a07
xiongxiong109 <444122662@qq.com>
2015-02-17 12:03:49 +0800

    changed the main method of the bear.js

--- bear.js
+++ bear.js
@@ -1,8 +1,20 @@
 /*
 Bear.js由bear公司自主研发的js框架库
 */
-var bear={
-	id:function(id){
-		return document.getElementById(id);
+var Bear=function(str){
+	var arr=[];
+	switch(typeof str){
+		case "function":
+		window.onload=str;break;
+		case "string":
+			var tag=str.charAt(0);
+			switch(tag){
+				case "#":
+				return document.getElementById(str.substring(1));
+				break;
+			}
 	}
+}
+var $=function(str){
+	return new Bear(str); 
 }
